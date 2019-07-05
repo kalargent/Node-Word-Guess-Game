2ca6706a497f26814ffd6627aa641973dd358d31
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-07-05 15:44:24 -0400

    initial repo and completed letter.js

---
+++ letter.js
@@ -0,0 +1,33 @@
+
+var Letter = function (letter) { 
+    this.letter = letter, 
+    this.guessed = false, 
+    this.display = function () {
+        if (this.guessed === false) { 
+            return "_ "; 
+        }
+        else return this.letter;
+    } 
+    this.check = function (value) { 
+        if (value === this.letter) {
+            this.guessed = true; 
+            return true; 
+        }
+        else return false; 
+
+    }
+
+}
+
+
+module.exports = Letter; 
+
+// TEST CODE 
+// var letter =  new Letter("a"); 
+
+// console.log (letter.display()); 
+// console.log (letter.check("b")); 
+// console.log (letter.display()); 
+
+
+ 
