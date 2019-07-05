1596897551bbc2c322c4dc28b2dd6bf5a885b446
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-07-05 16:45:34 -0400

    words displaying as underscores

--- word.js
+++ word.js
@@ -1,17 +1,24 @@
 var Letter = require("./letter");  
 
 function Word (word) { 
-    console.log(word);
+    // console.log(word);
     this.letterArray = []; 
 
     for (var i = 0; i < word.length; i++ ) { 
-        var Letter = new letter (word[i]); 
-        this.letterArray.push(word[i]); 
-        console.log (letterArray);
-        letter.display(); 
+        var letter = new Letter (word[i]); 
+        this.letterArray.push(letter); 
     }
     
+    this.display = function () {
+        var answer = ""; 
+        for (var i = 0; i < this.letterArray.length; i++) { 
+            answer += this.letterArray[i].display(); 
+        }
+        return (answer); 
+    }
 }
 
-Word("zipper");
-console.log(letter.display()); 
+var word = new Word ("zipper"); 
+
+console.log(word.display()); 
+
